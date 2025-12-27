1f3bc10257797d00e48ce0009d2b84cb42273ce5
drivejoshue <cpjoshuemc@gmail.com>
2025-12-27 17:01:05 -0600

    s

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,16 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 
-                    'resources/js/app.js', 
-                    'resources/css/adminkit/light.css',
-                    'resources/css/adminkit/dark.css',],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: [
+        'resources/css/adminkit/light.css',
+        'resources/css/adminkit/dark.css',
+        'resources/js/app.js',
+         'resources/css/app.css',
+        'resources/js/pages/dispatch.js',
+      ],
+      refresh: true,
+    }),
+  ],
 });
