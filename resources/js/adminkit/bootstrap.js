38536a77e94c28d1ae45b85b809e3598d12c3578
drivejoshue <cpjoshuemc@gmail.com>
2025-12-27 20:19:55 -0600

    Chore: remove unused adminkit Echo test file

--- resources/js/adminkit/bootstrap.js
+++ resources/js/adminkit/bootstrap.js
@@ -2,20 +2,8 @@ import axios from 'axios';
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-import Echo from 'laravel-echo';
-import Pusher from 'pusher-js';   // 👈 Reverb usa protocolo Pusher
-window.Pusher = Pusher;
-
-window.Echo = new Echo({
-  broadcaster: 'reverb',
-  key: import.meta.env.VITE_REVERB_APP_KEY ?? 'localkey',
-  wsHost: import.meta.env.VITE_REVERB_HOST ?? window.location.hostname,
-  wsPort: Number(import.meta.env.VITE_REVERB_PORT ?? 8080),
-  wssPort: Number(import.meta.env.VITE_REVERB_PORT ?? 8080),
-  forceTLS: false,                       // Reverb lo tienes en http
-  enabledTransports: ['ws', 'wss'],
-});
-
-// Prueba
-window.Echo.channel('driver.location.1')
-  .listen('.LocationUpdated', e => console.log('LocationUpdated:', e));
+const csrf = document.querySelector('meta[name="csrf-token"]')?.getAttribute('content');
+if (csrf) {
+  window.axios.defaults.headers.common['X-CSRF-TOKEN'] = csrf;
+  window.axios.defaults.headers.common['Accept'] = 'application/json';
+}
